939bc3951856dcbcd7c7d2b20cf3588522ec2ebd
zidandff <zidandevs@gmail.com>
2023-01-03 17:44:36 +0700

    Slicing header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1.5rem'
+    },
+    extend: {
+      colors: {
+        'dark-gray': 'hsl(0, 0%, 55%)',
+        'very-dark-gray': 'hsl(0, 0%, 41%)'
+      },
+      fontFamily: {
+        alata: 'Alata, sans-serif',
+        josefin: ['"Josefin Sans"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
